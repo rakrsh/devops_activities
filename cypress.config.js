9d1a25ea35e53a19696da795f6b37eb2fafb9afa
ravikr1 <ravikr731@gmail.com>
2024-10-20 19:50:53 +0530

    Added cypress conifg

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    e2e: {
+      setupNodeEvents(on, config) {
+        // implement node event listeners here
+      },
+      baseUrl: 'http://localhost:8080',  // replace with your application's base URL
+    },
+  }
+  
